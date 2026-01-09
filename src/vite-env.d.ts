62f03ff065cca2a7ae2743405f3d23c56e30b7df
Jedidiah Freiheit <jedidiahfreiheit@users.noreply.github.com>
2026-01-09 15:09:39 -0600

    Add Vite environment type definitions

---
+++ src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY: string
+  readonly VITE_GOOGLE_DRIVE_FOLDER_ID: string
+  readonly VITE_GOOGLE_CLIENT_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
